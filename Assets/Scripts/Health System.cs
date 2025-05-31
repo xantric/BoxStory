ebcf115667f4124d064f174d979b7231b7208ef7
Xantric <ishankamboj35@gmail.com>
2025-05-31 13:00:31 +0530

    added water splash effect

--- Assets/Scripts/Health System.cs
+++ Assets/Scripts/Health System.cs
@@ -5,6 +5,8 @@ public class HealthSystem : MonoBehaviour
     [SerializeField]
     float health = 100f; // Default health value
     public float maxHealth = 100f; // Maximum health value
+    public ParticleSystem _deathEffect;
+    public Animator _animator;
     void Start()
     {
         health = maxHealth; // Initialize health to max health at the start
@@ -15,7 +17,8 @@ public class HealthSystem : MonoBehaviour
         Debug.Log($"Health reduced of {gameObject.name}. Current health: {health}");
         if (health <= 0f)
         {
-            Die(); // Call the Die method if health is zero or below
+            _animator.SetTrigger("Death");
+            // Die(); // Call the Die method if health is zero or below
         }
     }
     public void Die()
@@ -23,5 +26,13 @@ public class HealthSystem : MonoBehaviour
         // Handle death logic here, such as playing an animation or destroying the object
         Debug.Log("Character has died.");
         Destroy(gameObject); // Destroy the game object when health reaches zero
+    }
+
+    public void PlayDeathParticleEffect()
+    {
+        if (_deathEffect != null)
+        {
+            _deathEffect.Play();
+        }
     }
 }
